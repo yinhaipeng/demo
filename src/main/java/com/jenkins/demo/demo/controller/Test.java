c958c5c1a68ae52fbddd1ea37fc5069ffceca46d
yinhp <88888888>
2021-01-27 17:58:38 +0800

    提交

---
+++ src/main/java/com/jenkins/demo/demo/controller/Test.java
@@ -0,0 +1,21 @@
+package com.jenkins.demo.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author YanChengLong
+ * @date 2019/7/25
+ * @desc 测试
+ */
+@RestController
+public class Test {
+
+    @RequestMapping("/test")
+
+    public String test() {
+        return "aaaa";
+    }
+
+}
